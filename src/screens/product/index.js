4264a24a4f4b4706c9a049b12f72fbf44fb32b45
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-09-21 22:29:56 -0300

    first commit

---
+++ src/screens/product/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+const Product = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Producto</Text>
+    </View>
+  );
+};
+
+export default Product;
---
+++ src/screens/products/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+const Productos = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Productos</Text>
+    </View>
+  );
+};
+
+export default Productos;
